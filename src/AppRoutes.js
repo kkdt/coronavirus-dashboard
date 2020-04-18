a718a8631de2bcc9b92f10cb8be8f7b42139a3cd
thinh ho <me@kkdt.one>
2020-04-17 23:12:31 -0400

    Updating README; Setting new tab page to DefaultContent

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -2,8 +2,8 @@ import React from 'react';
 import {Switch} from "react-router-dom";
 import ReportedCasesController from "./controllers/ReportedCasesController";
 import ApplicationRoute from "./components/ApplicationRoute/ApplicationRoute";
-import PageNotFound from "./pages/PageNotFound/PageNotFound";
 import HomePage from "./pages/HomePage";
+import DefaultContent from "./pages/DefaultContent/DefaultContent";
 
 function AppRoutes(props) {
 
@@ -12,7 +12,9 @@ function AppRoutes(props) {
             <ApplicationRoute exact path="/" component={HomePage}/>,
             <ApplicationRoute path="/home" component={HomePage}/>,
             <ApplicationRoute path="/reported" component={ReportedCasesController}/>,
-            <ApplicationRoute component={PageNotFound} />
+
+            // Opening a new tab will default to this page
+            <ApplicationRoute component={DefaultContent} />
         </Switch>
     );
 }
